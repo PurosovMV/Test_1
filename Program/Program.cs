70481d603c35a4928e865da16789d73ed1ca9108
PurosovMV <mihail.purosov@hotmail.com>
2023-02-16 22:32:09 +0300

    Добавлена папка с программой

---
+++ Program/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Функция ввода исходного массива.
+string[] InputArray()
+{
+    Console.WriteLine("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+//Функция нахождения и вывода элементов массива длина которых меньше или равна "n"
+void ProductArray(string[] array, int n)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+
+}
+
+string[] array = InputArray();
+ProductArray(array, 3);
